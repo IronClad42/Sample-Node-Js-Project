f7c5205b22d9f4964f58979e2556fcfd8a62337c
IronClad42 <krishnabhujbal176@gmail.com>
2026-01-14 23:06:48 +0530

    GitHub Token Password

--- index2.js
+++ index2.js
@@ -1,2 +1,6 @@
 console.log("kay Error aahe te sanga");
 console.log("Mala");
+
+// GitHub => Password aahe ha
+
+// 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
