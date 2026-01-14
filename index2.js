e02fe57cf01897b868ce6d754482c301df045179
IronClad42 <krishnabhujbal176@gmail.com>
2026-01-14 23:09:54 +0530

    github password

--- index2.js
+++ index2.js
@@ -1,6 +1,5 @@
 console.log("kay Error aahe te sanga");
 console.log("Mala");
 
-// GitHub => Password aahe ha
+// GitHub =>
 
-// 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
